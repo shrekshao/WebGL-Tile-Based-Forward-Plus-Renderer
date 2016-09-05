f37cc94daf08881b56b644cde2d6c2091010edb5
shrekshao <shrekshao@gmail.com>
2016-09-04 22:49:50 -0400

    depthPrepass refactor

---
+++ js/forwardPlusRenderer/pass/depthPrepass.js
@@ -0,0 +1,39 @@
+var ForwardPlusRenderer = ForwardPlusRenderer || {};
+(function() {
+    'use strict';
+
+    var FPR = ForwardPlusRenderer;
+
+    FPR.pass.depthPrepass.fboInit = function () {
+        console.log("depthPrepass FBO Init");
+
+        var gl = FPR.gl;
+        var width = FPR.width;
+        var height = FPR.height;
+
+        var self = FPR.pass.depthPrepass;
+
+        // Create the depth texture
+        var depthTexture = self.depthTexture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, depthTexture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.DEPTH_COMPONENT, width, height, 0, gl.DEPTH_COMPONENT, gl.UNSIGNED_SHORT, null);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+
+        var framebuffer = self.framebuffer = gl.createFramebuffer();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+        //gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, colorTexture, 0);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.TEXTURE_2D, depthTexture, 0);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    }
+    
+    FPR.pass.depthPrepass.fboBind = function () {
+        var gl = FPR.gl;
+        gl.bindFramebuffer(gl.FRAMEBUFFER, FPR.pass.depthPrepass.framebuffer);
+    }
+
+
+})();
