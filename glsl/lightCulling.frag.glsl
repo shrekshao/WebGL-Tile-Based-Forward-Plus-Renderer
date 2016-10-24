f622e5ffe95f684476c1834bc90783a1c8ccecae
shrekshao <shrekshao@gmail.com>
2016-10-23 20:26:06 -0400

    [lightCulling.frag.glsl] tileIdx test, and pick only one thread per tile

--- glsl/lightCulling.frag.glsl
+++ glsl/lightCulling.frag.glsl
@@ -4,6 +4,8 @@
 precision highp float;
 precision highp int;
 
+#define TILE_SIZE 16
+
 varying vec2 v_uv;
 
 uniform int u_numLights; 
@@ -22,53 +24,41 @@ uniform sampler2D u_tileFrustumPlanesTexture;   // RGB, store frustum planes of 
 uniform sampler2D u_depthTexture;
 
 void main() {
-    vec2 tileIdx = v_uv; 
+    
 
     // vec2 tileInfo = texture2D(u_tileTexture, tileIdx).xy;  // [offset, size]
 
 
     // // debug, render u_lightPositionTexture --------------------
-    // // float lightIdx = v_uv.y * float(u_textureWidth) + v_uv.x;
-    float threadIdx = ((gl_FragCoord.y - 0.5) * float(u_textureWidth) + (gl_FragCoord.x- 0.5));
-    // float lightIdx = texture2D(u_lightIndexTexture, vec2(threadIdx, 0.0));
-
-
-
-    // float lightIdx = texture2D(u_lightIndexTexture, vec2(v_uv.y * float(u_textureWidth) + v_uv.x, 0.0));
-    if (threadIdx < float(u_numLights))
-    // if (threadIdx < 1000.0)
-    // if (threadIdx < 1.0)
-    {
-    //     // vec3 lightPos = texture2D(u_lightPositionTexture, vec2(lightIdx / float(u_numLights))).xyz;
-    //     vec3 lightPos = texture2D(u_lightPositionTexture, vec2(0.5, 2)).xyz;
-        vec3 lightPos = texture2D(u_lightPositionTexture, vec2(threadIdx / float(u_numLights), 0.5)).xyz;
-
-    //     // if (lightPos.y > 0.5)
-    //     // {
-    //     //     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    //     // }
-    //     // else
-    //     // {
-    //     //     gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-    //     // }
+    // float threadIdx = ((gl_FragCoord.y - 0.5) * float(u_textureWidth) + (gl_FragCoord.x - 0.5));
+    // if (threadIdx < float(u_numLights))
+    // {
+    //     vec3 lightPos = texture2D(u_lightPositionTexture, vec2((threadIdx) / float(u_numLights) , 0.5)).xyz;
+        
     //     gl_FragColor = vec4(lightPos, 1.0);
-        
-        gl_FragColor = vec4(lightPos, 1.0);
-        // gl_FragColor = vec4(threadIdx, 0.0, 0.0, 1.0);
-        // gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-        
-    }
+    //     // gl_FragColor = vec4(vec3(lightPos.y), 1.0);
+    // }
+    // // -------------------------------------
 
-    // gl_FragColor = vec4(v_uv, 0.0, 1.0);
-    // gl_FragColor = vec4(
-    //         gl_FragCoord.x / float(u_textureWidth), 
-    //         gl_FragCoord.y / float(u_textureHeight), 
-    //         0.0, 
-    //         1.0);
-    
 
 
     // find current threadIdx and tileIdx
+    ivec2 pixelIdx = ivec2(gl_FragCoord.xy);    //floor
+    ivec2 tileIdx = pixelIdx / TILE_SIZE;
+
+    // use exact one thread per tile to gather lights (kind of difficult...)
+    if ( pixelIdx == tileIdx * TILE_SIZE)
+    {
+        // working thread for this tile
+        gl_FragColor = vec4(1.0);
+    }
+    else
+    {
+        // other idle threads
+        // should not operate to avoid race conditions
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+
 
     // for loop light
     //  if pass frustum hit test
